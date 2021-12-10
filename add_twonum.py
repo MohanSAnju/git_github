ade4984df9715adabea266da1929750d15364eec
MohanSAnju <mohan.pidugu95@gmail.com>
2021-12-11 02:50:11 +0530

    handled exception normally

--- add_twonum.py
+++ add_twonum.py
@@ -1,11 +1,18 @@
 #Add Two numbers program in Python....
+#Division those two numbers also
 #now i have to write this program in exception way
 try:
     x=int(input("enter first num:"))
     y=int(input("enter second num:"))
-   
+    add=x+y
+    if y==0:
+        print("ZeroDivisionError")
+    div=x/y
 except(ValueError):
     print("please give proper input")
+except(ZeroDivisionError):
+    print()
 else:
-    res=x+y
-    print("res:",res)
+    print("addion of two numbers is",add)
+    print("Division of two numbers is:",div)
+    
