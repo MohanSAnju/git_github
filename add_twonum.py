620e0ca77524c2774bf53bdb1a2846fd96dcb0e4
MohanSAnju <mohan.pidugu95@gmail.com>
2021-12-11 02:01:55 +0530

    try and except code

--- add_twonum.py
+++ add_twonum.py
@@ -1,7 +1,9 @@
 #Add Two numbers program in Python....
-x=int(input("enter first num:"))
-y=int(input("enter second num:"))
-if x!=0 or y!=0:
-    print("result",x+y)
-else:
-    print("please give other than 0 as a input")
+#now i have to write this program in exception way
+try:
+    x=int(input("enter first num:"))
+    y=int(input("enter second num:"))
+    res=x+y
+    print("res:",res)
+except(ValueError):
+    print("please give proper input")
