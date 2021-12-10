d6333d5c9a463b1a446af60e8322d66f980eb5d8
MohanSAnju <mohan.pidugu95@gmail.com>
2021-12-11 03:21:32 +0530

    zerodivision error handled is added

--- add_twonum.py
+++ add_twonum.py
@@ -6,8 +6,11 @@ try:
     y=int(input("enter second num:"))
     add=x+y
     div=x/y
+except(ZeroDivisionError):
+    print("ZeroDivisionError: 'please enter other than zero as first number' ")        
 except(ValueError):
     print("please give proper input")
 else:
     print("addion of two numbers is",add)
     print("Division of two numbers is:",div)
+    
